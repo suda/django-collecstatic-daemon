b5c03e2b126c9135e70363fab1af2fc24f384549
Wojtek Siudzinski <admin@suda.pl>
2020-01-10 13:45:04 +0100

    Replace collecstatic with collectstatic

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""django_collecstatic_daemon URL Configuration
+"""django_collectstatic_daemon URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_collecstatic_daemon project.
+WSGI config for django_collectstatic_daemon project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
